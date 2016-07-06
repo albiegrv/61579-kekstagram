86324aa496fc19165d534782f4cc54be22ae8131
insanegrv <insanegrv@gmail.com>
2016-07-07 01:17:00 +0300

    Check

---
+++ build/js/check.js
@@ -0,0 +1,24 @@
+function getMessage(a, b) {
+  if (typeof a === 'boolean') {
+    if (a) {
+      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+    } else {
+      return 'Переданное GIF-изображение не анимировано';
+    }
+  } else if (typeof a === 'number') {
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
+  } else if (Array.isArray(a)) {
+    if (Array.isArray(b)) {
+      var square = 0;
+      for (var i = 0; i < a.length; i++) {
+        square += a[i] + b[i];
+      }
+      return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
+    } else {
+      var sum = a.reduce(function(temp, current) {
+        return temp + current;
+      }, 0);
+      return 'Количество красных точек во всех строчках изображения: ' + sum;
+    }
+  }
+}
